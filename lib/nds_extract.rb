ae56c998621d33c31feffe319eb4adcb0a95d562
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-26 20:00:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,7 +54,8 @@ def movies_with_director_key(name, movies_collection)
     {
       :name => "The invencibles",
       :worldwide_gross => 300000,
-      :RUBY_RELEASE_DATE => 
+      :RUBY_RELEASE_DATE => 2008,
+      :studio => 
       
       
       
