3e00d5bcd83dbf13660d289ae1e6a420af585030
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-26 19:56:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,7 +49,12 @@ def movies_with_director_key(name, movies_collection)
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
   
-  movies
+  movies = [
+    
+    {:name => "The invencibles"}
+    
+    
+    ]
   
   
 
